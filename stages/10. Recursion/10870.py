45fce7a1c15bfa39e0e2ed1b6ae5a0b579414773
limjisuu <dlawltn1999@naver.com>
2020-10-30 18:16:39 +0900

    10870 피보나치 수 5

---
+++ stages/10. Recursion/10870.py
@@ -0,0 +1,12 @@
+import sys
+num = int(sys.stdin.readline())
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+print(fibo(num))
