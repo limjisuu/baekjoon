b5c9273aa0837dc941dc22d0b46e4838500a7ce3
limjisuu <dlawltn1999@naver.com>
2021-01-22 00:52:46 +0900

    1932 정수 삼각형

---
+++ stages/14. Dynamic Programming/1932.py
@@ -0,0 +1,17 @@
+import sys
+n = int(sys.stdin.readline())
+triangle = []
+
+for i in range(n):
+    triangle.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, n):
+    for j in range(len(triangle[i])):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][0]
+        elif j == len(triangle[i]) - 1:
+            triangle[i][j] += triangle[i-1][-1]
+        else:
+            triangle[i][j] += + max(triangle[i-1][j-1], triangle[i-1][j])
+
+print(max(triangle[n-1]))
