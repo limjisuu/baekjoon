02b2d2b2aabbcda00f67445a7bc07acc9d3f13df
Jisu Lim <limjisuu@korea.ac.kr>
2021-06-11 15:45:32 +0900

    9657 돌 게임 3

---
+++ algorithms/Dynamic programming/9657.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+dp = [0 for _ in range(1001)]
+dp[1] = dp[3] = dp[4] = 1
+for i in range(5, n+1):
+    if dp[i-1] and dp[i-3] and dp[i-4]:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+print("SK") if dp[n] else print("CY")
