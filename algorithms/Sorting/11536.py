0400728e79dabc0c4f1875e0d787be5e8d6092f2
Jisu Lim <limjisuu@korea.ac.kr>
2021-09-17 17:23:24 +0900

    11536 줄 세우기

---
+++ algorithms/Sorting/11536.py
@@ -0,0 +1,8 @@
+n = int(input())
+names = [input() for _ in range(n)]
+if sorted(names) == names:
+    print("INCREASING")
+elif sorted(names, reverse=True) == names:
+    print("DECREASING")
+else:
+    print("NEITHER")
